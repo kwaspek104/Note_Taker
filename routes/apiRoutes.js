96474a7778bb890b09a6d8b9a26977496fcdb490
kwaspek104 <kasper.kuzmicki@yahoo.com>
2021-01-02 19:42:16 -0500

    Another commit.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,25 +6,27 @@ const path = require('path');
 const readFileasync = util.promisify(fs.readFile);
 
 
-router.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-  });
+// router.get('/notes', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../public/notes.html'));
+//   });
 
-  router.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+//   router.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
-    router.get("/api/notes", function (req, res) {
+    router.get("/notes", function (req, res) {
     readFileasync("../Develop/db/db.json", "utf8").then(data)
     });
 
-    router.post("/api/notes", function(req, res) {
+    router.post("/notes", function(req, res) {
         // POST /api/notes - Should receive a new note to save 
         // on the request body, add it to the db.json file, 
         // and then return the new note to the client.
+        console.log("POST-ROUTE HAS BEEN HIT")
         var newBody = req.body
         data.push(req.body);
         res.json(newBody);
+        console.log(req.body)
     });
     
     //then export it
